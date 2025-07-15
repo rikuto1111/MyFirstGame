da8f7ee2064b969963c729a0cd641c7aed84a190
240002 <240002@E0403-01>
2025-07-15 15:06:17 +0900

    プロジェクト ファイルを追加します。

---
+++ MyFirstGame/Dice.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Quad.h"
+
+class Dice :
+	public Quad
+{
+public:
+	Dice();
+	~Dice();
+	HRESULT Initialize() override;
+	void Draw(XMMATRIX& worldMatrix)override;
+	void Release();
+
+
+};
+
---
+++ MyFirstGame/Quad.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Direct3D.h"
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+struct CONSTANT_BUFFER
+{
+	DirectX::XMMATRIX matWVP; //world*view*projectionの行列
+};
+
+//頂点情報
+struct VERTEX
+{
+	XMVECTOR pos;
+	XMVECTOR uv;
+};
+class Texture;
+
+class Quad
+{	
+public:
+	Quad();
+	virtual ~Quad();
+	virtual HRESULT Initialize();
+	virtual void Draw(XMMATRIX& worldMatrix);
+	void Release();
+protected:
+	ID3D11Buffer* pVertexBuffer_;	//頂点バッファ
+	ID3D11Buffer* pIndexBuffer_;	//頂点バッファ
+	ID3D11Buffer* pConstantBuffer_;	//コンスタントバッファ
+	Texture* pTexture_;
+};
