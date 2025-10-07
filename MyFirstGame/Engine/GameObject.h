af3e62f73dde3fd3f1c31c675ec69144593fe698
240002 <240002@E0403-01>
2025-10-07 15:07:46 +0900

    GameObjectとRootJobを追加した

---
+++ MyFirstGame/Engine/GameObject.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <list>
+#include <string>
+#include "Transform.h"
+
+
+using std::string;
+using std::list;
+
+class GameObject
+{
+	list<GameObject*> childList_;
+	Transform	transform_;
+	GameObject* pParent_;
+	string	objectName_;
+
+public:
+	GameObject();
+	GameObject(GameObject* parent, const std::string& name);
+	~GameObject();
+
+	virtual void Initialize() = 0;
+	virtual void Update() = 0;
+	virtual void Draw() = 0;
+	virtual void Release() = 0;
+};
---
+++ MyFirstGame/Engine/RootJob.cpp
@@ -0,0 +1,25 @@
+#include "RootJob.h"
+
+RootJob::RootJob(GameObject* parent)
+{
+}
+
+RootJob::~RootJob()
+{
+}
+
+void RootJob::Initialize()
+{
+}
+
+void RootJob::Update()
+{
+}
+
+void RootJob::Draw()
+{
+}
+
+void RootJob::Release()
+{
+}
